8327ec0e8af18f627079a874d0d49d0786235395
treblamaker <86634184+treblamaker@users.noreply.github.com>
2021-07-18 15:03:08 -0700

    ifthisworks

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 function myFunction() {
     document.getElementById("ent").innerHTML = "I was clicked."
 }
+function sendEmail(name,email,message) {
+    email.send({
+        Host:"smtp.gmail.com",
+        Username:"aosongco@gmail.com",
+        Password:"vphefbjicotergkc",
+        To: "aosongco@gmail.com",
+        From:"aosongco@gmail.com",
+        Subject: '${name} sent you a message',
+        Body: 'Name: ${name} <br/> Email:${email} ,br/> Message: ${message}',
+    }).then((message) => alert("mail sent successfully"))
+}
